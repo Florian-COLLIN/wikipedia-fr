e1de8eb30da0d6fa2dfecbb76b8358555232f8a7
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2020-12-14 23:16:08 +0100

    Update from GitHub Desktop.

--- gadgets/filmo/test.js
+++
@@ -1,7 +0,0 @@
-var cpeSpecialPage = "Spécial:Filmo";
-
-$(function () {
-	mw.loader.using( 'mediawiki.util', function () {
-		mw.util.addPortletLink('p-tb', mw.config.get("wgArticlePath").replacee(/\$1/, cpeSpecialPage), "Filmographie", "r-cpe", "Filmographie");
-	});
-});
